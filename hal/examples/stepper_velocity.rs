5d1d015ce436fea8b618af2e79dee491ae39e59e
Scott Hyndman <scotty.hyndman@gmail.com>
2022-11-23 02:37:54 -0500

    Demonstrate use of micro-stepping pins

--- hal/examples/stepper_velocity.rs
+++ hal/examples/stepper_velocity.rs
@@ -6,7 +6,7 @@ use aa_hal::{
 };
 use anyhow::{anyhow, Result};
 use rppal::gpio::Gpio;
-use stepper::{drivers::a4988::A4988, ramp_maker, Direction, Stepper};
+use stepper::{drivers::a4988::A4988, ramp_maker, step_mode::StepMode16, Direction, Stepper};
 
 const RATE_1MHZ: u32 = 1_000_000;
 
@@ -20,19 +20,30 @@ fn main() -> Result<()> {
         .init();
 
     let gpio = Gpio::new()?;
-    let step_pin = gpio.get(16)?.into_output();
+    let step_pin = gpio.get(20)?.into_output();
     let direction_pin = gpio.get(21)?.into_output();
-    let mut timer = Timer::<RATE_1MHZ>::new_non_blocking();
 
+    let reset_pin = gpio.get(16)?.into_output();
+    let ms1_pin = gpio.get(26)?.into_output();
+    let ms2_pin = gpio.get(19)?.into_output();
+    let ms3_pin = gpio.get(13)?.into_output();
+
+    let mut timer = Timer::<RATE_1MHZ>::new_blocking();
     let stepper = Stepper::from_driver(A4988::new())
         .enable_direction_control(direction_pin, Direction::Forward, &mut timer)
+        .map_err(|e| anyhow!("{:?}", e))?
+        .enable_step_mode_control(
+            (reset_pin, ms1_pin, ms2_pin, ms3_pin),
+            StepMode16::Full,
+            &mut timer,
+        )
         .map_err(|e| anyhow!("{:?}", e))?
         .enable_step_control(step_pin);
 
     let profile = ramp_maker::Flat::<f32>::new();
     let mut motion_ctrl =
         stepper.enable_motion_control((timer.clone(), profile, FloatDelayToTicks));
-    for degs_per_second in (1..=20).map(|i| i as f32 * 5.0) {
+    for degs_per_second in (10..=30).map(|i| i as f32 * 4.0) {
         let degs_per_tick = degs_per_second / RATE_1MHZ as f32;
         info!("ROTATE AT {}°/s ({}°/tick)", degs_per_second, degs_per_tick);
         let steps_per_tick = degs_per_tick / 2.0;
@@ -54,6 +65,7 @@ fn main() -> Result<()> {
             .set_direction(Direction::Backward, &mut timer)
             .wait()
             .map_err(|e| anyhow!("{:?}", e))?;
+        sleep_nanos(2 * 1_000); // Sleep 2µs
         motion_ctrl
             .move_to_position(steps_per_tick, 0)
             .wait()
