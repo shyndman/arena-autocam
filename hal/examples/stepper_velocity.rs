84597775356a0dc2e52f94b1cf2bdaebe56c2a26
Scott Hyndman <scotty.hyndman@gmail.com>
2022-11-23 02:53:09 -0500

    Tidy up the stepper velocity example a little

--- hal/examples/stepper_velocity.rs
+++ hal/examples/stepper_velocity.rs
@@ -29,7 +29,7 @@ fn main() -> Result<()> {
     let ms3_pin = gpio.get(13)?.into_output();
 
     let mut timer = Timer::<RATE_1MHZ>::new_blocking();
-    let stepper = Stepper::from_driver(A4988::new())
+    let mut stepper = Stepper::from_driver(A4988::new())
         .enable_direction_control(direction_pin, Direction::Forward, &mut timer)
         .map_err(|e| anyhow!("{:?}", e))?
         .enable_step_mode_control(
@@ -38,21 +38,23 @@ fn main() -> Result<()> {
             &mut timer,
         )
         .map_err(|e| anyhow!("{:?}", e))?
-        .enable_step_control(step_pin);
+        .enable_step_control(step_pin)
+        .enable_motion_control((
+            timer.clone(),
+            ramp_maker::Flat::<f32>::new(),
+            FloatDelayToTicks,
+        ));
 
-    let profile = ramp_maker::Flat::<f32>::new();
-    let mut motion_ctrl =
-        stepper.enable_motion_control((timer.clone(), profile, FloatDelayToTicks));
     for degs_per_second in (10..=30).map(|i| i as f32 * 4.0) {
         let degs_per_tick = degs_per_second / RATE_1MHZ as f32;
         info!("ROTATE AT {}°/s ({}°/tick)", degs_per_second, degs_per_tick);
         let steps_per_tick = degs_per_tick / 2.0;
 
-        motion_ctrl
+        stepper
             .set_direction(Direction::Forward, &mut timer)
             .wait()
             .map_err(|e| anyhow!("{:?}", e))?;
-        motion_ctrl
+        stepper
             .move_to_position(steps_per_tick, 30)
             .wait()
             .map_err(|e| anyhow!("{:?}", e))?;
@@ -61,12 +63,12 @@ fn main() -> Result<()> {
         sleep_nanos(100 * 1_000_000); // Sleep 100ms
 
         // Reset to original position
-        motion_ctrl
+        stepper
             .set_direction(Direction::Backward, &mut timer)
             .wait()
             .map_err(|e| anyhow!("{:?}", e))?;
         sleep_nanos(2 * 1_000); // Sleep 2µs
-        motion_ctrl
+        stepper
             .move_to_position(steps_per_tick, 0)
             .wait()
             .map_err(|e| anyhow!("{:?}", e))?;
