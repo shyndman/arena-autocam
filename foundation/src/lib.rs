fa926c6ec470a1c694e6d4dd762e594ae5994447
Scott Hyndman <scotty.hyndman@gmail.com>
2022-11-28 15:33:41 -0500

    Expose new utils in foundation lib

--- foundation/src/lib.rs
+++ foundation/src/lib.rs
@@ -1,3 +1,7 @@
+pub mod clock;
 pub mod env;
+pub mod duration;
 pub mod prelude;
+pub mod spring;
+pub mod thread;
 pub mod tracing;
--- foundation/src/prelude.rs
+++ foundation/src/prelude.rs
@@ -1,3 +1,5 @@
 pub use indoc::indoc;
 #[allow(unused_imports)]
 pub use tracing::{debug, error, info, trace, warn};
+
+pub use crate::duration::DurationExtTrait;
